3647df54b059d8fc6c2bf54efc16c92ace56f614
mmalan <matt.malan@gmail.com>
2015-10-13 21:38:23 -0600

    YouGotNoCakeRepo

---
+++ YouGotNoCakeMC/src/Model/Inventory.java
@@ -0,0 +1,86 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Model;
+
+import java.io.Serializable;
+import java.util.Objects;
+
+/**
+ *
+ * @author MattMalan
+ */
+public class Inventory implements Serializable{
+    
+    // class instance variables
+    private String item;
+    private String type;
+    private String attribute;
+
+    public Inventory() {
+    }
+     
+    public String getItem() {
+        return item;
+    }
+
+    public void setItem(String item) {
+        this.item = item;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public String getAttribute() {
+        return attribute;
+    }
+
+    public void setAttribute(String attribute) {
+        this.attribute = attribute;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 5;
+        hash = 53 * hash + Objects.hashCode(this.item);
+        hash = 53 * hash + Objects.hashCode(this.type);
+        hash = 53 * hash + Objects.hashCode(this.attribute);
+        return hash;
+    }
+
+    @Override
+    public String toString() {
+        return "Inventory{" + "item=" + item + ", type=" + type + ", attribute=" + attribute + '}';
+    }
+
+        
+    @Override
+    public boolean equals(Object obj) {
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        final Inventory other = (Inventory) obj;
+        if (!Objects.equals(this.item, other.item)) {
+            return false;
+        }
+        if (!Objects.equals(this.type, other.type)) {
+            return false;
+        }
+        if (!Objects.equals(this.attribute, other.attribute)) {
+            return false;
+        }
+        return true;
+    }
+    
+    
+}
