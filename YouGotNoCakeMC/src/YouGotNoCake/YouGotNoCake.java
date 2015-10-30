6e0c58d26219756bd8db0a0a1313a724c0e59aea
claytonharwood <bigfatweb@gmail.com>
2015-10-29 20:29:41 -0600

    10/29/15 - updated for team assignment lesson 6

--- YouGotNoCakeMC/src/YouGotNoCake/YouGotNoCake.java
+++ YouGotNoCakeMC/src/YouGotNoCake/YouGotNoCake.java
@@ -5,16 +5,15 @@
  */
 package yougotnocake;
 
-//import byui.cit260.youGotNoCake2.model.Inventory;
-
+import Control.ProgramControl;
 import Model.Game;
 import Model.Player;
 import Model.Inventory;
 import Model.Location;
 import Model.Map;
 import Model.Progress;
+import view.StartProgramView;
 
-//import byui.cit260.youGotNoCake2.model.Player;
 
 /**
  *
@@ -22,60 +21,34 @@ import Model.Progress;
  */
 public class YouGotNoCake {
 
-    /**
-     * @param args the command line arguments
-     */
+    private static Game currentGame = null;
+    private static Player player = null;
+    
     public static void main(String[] args) {
-        Player playerOne = new Player();
+        //create StartProgramingView and start the porgram
+        StartProgramView startProgramView = new StartProgramView();
+        startProgramView.startProgram();
         
-        playerOne.setName("Fred Rogers");
-        playerOne.setBestTime(7.00);
-        
-        String playerInfo = playerOne.toString();
-        System.out.println(playerInfo);
-        
-        
-        Inventory inventoryOne = new Inventory();
-        
-        inventoryOne.setItem("2");
-        inventoryOne.setType("Ingredients");
-        inventoryOne.setAttribute("Sugar");
-    
-        String inventoryInfo = inventoryOne.toString();
-        System.out.println(inventoryInfo);
-        
-        Progress youWin = new Progress();
-        Progress youLoose = new Progress();
-        
-        youWin.setYouWin("You Got Cake!");
-        youLoose.setYouLoose("You Got no cake!");
-        
-        String playerProgress = youWin.toString();
-        System.out.println(playerProgress);
-        
-        Map rowCount = new Map();
-        
-        rowCount.setRowCount("New Map Area!");
-        
-        String areaMap = rowCount.toString();
-        System.out.println(areaMap);
-        
-        Location currentLocation = new Location();
-        
-        currentLocation.setCurrentLocation("You are here!");
-        
-        String mapLocation = currentLocation.toString();
-        System.out.println(mapLocation);
-        
-        Game playerName = new Game();
-        
-        playerName.setPlayerName("Name Goes here");
-        
-        String newGame = playerName.toString();
-        System.out.println(newGame);
-    
+    }
+
+    public static Game getCurrentGame() {
+        return currentGame;
+    }
+
+    public static void setCurrentGame(Game currentGame) {
+        YouGotNoCake.currentGame = currentGame;
+    }
+
+    public static Player getPlayer() {
+        return player;
+    }
+
+    public static void setPlayer(Player player) {
+        YouGotNoCake.player = player;
     }
     
     
+
 }
+
 
