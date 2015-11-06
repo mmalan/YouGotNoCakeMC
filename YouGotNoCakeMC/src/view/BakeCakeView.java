e73f6365e7c23889cb274c726fdd912e673764f6
claytonharwood <bigfatweb@gmail.com>
2015-11-05 21:31:29 -0700

    11/5/15 - individual assignment

---
+++ YouGotNoCakeMC/src/view/BakeCakeView.java
@@ -0,0 +1,74 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package view;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author Clayton
+ */
+public class BakeCakeView {
+    
+    private final String MENU = "\n"
+                + "\n----------------------------------------------------------"
+                + "\n| Cake Ingredients                                       |"
+                + "\n----------------------------------------------------------"
+                + "\nSugar" //will display quantity
+                + "\nEggs" //will display quantity
+                + "\nButta" //will display quantity
+                + "\nFlour" //will display quantity
+                + "\nback" //will take you back to inventory menu
+                + "\n----------------------------------------------------------";
+    
+    
+     public void displayIngredients() {
+                
+        String input = null;
+        do{
+            System.out.println(MENU); // display the main menu
+            
+            input = this.getCake(); // get the user's selection
+            //String answer = input.(yes); // get first character of string
+            
+            this.bakeCake(input); // do action based on selection
+        }while (input != "back"); // an selection is not "Exit"
+    }
+    
+    public String getCake() {
+        boolean valid = false; //indicates if the name has been retrieved
+        String entry = null;
+        Scanner keyboard = new Scanner(System.in); //keyboard input stream
+        
+        while(!valid) { //while a valid name has not been retrieved
+            
+            //prompt for menu selection
+            System.out.println("Would you like to bake a cake? yes or no?");
+            
+             //get selection from the keyboard and trim off the blanks
+            entry = keyboard.nextLine();
+            entry = entry.trim();
+            entry = entry.toLowerCase();
+            
+            if (entry.length() < 2){
+                System.out.println("Invalid name - enter yes or no");
+                continue; //and repeat again
+            }
+            break; 
+        }
+        
+        return entry ; //return the selection
+    }
+   
+   public void bakeCake(String input){
+       
+        if(input == "yes"){
+            System.out.println("Sweet, you have made a cake and the sugar is rushing to your head!");
+        }else{
+            System.out.println("You do not have enough ingredients yet");
+           }
+       }
+   }
